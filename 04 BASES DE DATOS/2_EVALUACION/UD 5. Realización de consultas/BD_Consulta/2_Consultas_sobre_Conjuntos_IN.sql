c2ae1e27df53ae28a464b7250017c9e36e08b8cc
zono32 <masterzono32@gmail.com>
2024-01-15 22:03:13 +0100

    base datos 15/01/24

--- 04 BASES DE DATOS/2_EVALUACION/UD 5. Realización de consultas/BD_Consulta/2_Consultas_sobre_Conjuntos_IN.sql
+++ 04 BASES DE DATOS/2_EVALUACION/UD 5. Realización de consultas/BD_Consulta/2_Consultas_sobre_Conjuntos_IN.sql
@@ -1,68 +1,77 @@
 -- Base de datos Tienda Informática (2 tablas)
-
+use tienda;
 -- 1 Muestra los datos de los fabricantes cuyos códigos van del 2 al 7.
-
+select * from fabricante where codigo between 2 and 7;
 -- 2 Muestra los datos de los fabricantes cuyos códigos no sean el 2, 4 y 6.
-
+select * from fabricante where not codigo  in  (2, 4, 6);
 -- 3 Muestra los datos de los productos cuyos códigos son 1, 3, 4, y 7.
-
+select * from producto where codigo in (1, 3, 4, 7);
 
 -- Base de datos Gestión de Empleados (2 tablas)
-
+use empleados;
 -- 1 Muestra los datos de los departamentos Desarrollo, Sistemas y Contabilidad.
 
+select * from departamento where nombre in ('Desarrollo',  'Sistemas', 'Contabilidad');
 -- 2 Muestra los datos de los departamentos cuyo presupuesto no sea 0.
 
+select * from departamento where presupuesto != 0;
+select * from departamento where not presupuesto in (0);
+
 -- 3 Muestra los datos de los empleados cuyo departamento sea el 2, 4 ó 5.
+select * from empleado where codigo_departamento in (2, 4, 5);
 
 -- 4 Muestra los datos de los empleados apellidados Ruiz o Salas.
+select * from empleado where apellido1 in ('Ruiz', 'Salas') OR apellido2 in ('Ruiz', 'Salas');
 
 
--- 1 Base de datos Gestión de Ventas (3 tablas)
 
--- 2 Muestra los datos de los clientes que se llamen Maria o Adela.
-
--- 3 Muestra los datos de los comerciales que tengan como primer apellido Gómez o Domínguez.
-
--- 4 Muestra los datos de los pedidos realizados por los clientes con los códigos 1, 2 y 6.
-
--- 5 Muestra los datos de los pedidos realizados por los comerciales cuyo códigos no sean 1, 2 ó 6.
-
+--  Base de datos Gestión de Ventas (3 tablas)
+use ventas;
+-- 1 Muestra los datos de los clientes que se llamen Maria o Adela.
+select * from cliente where nombre in ('Maria', 'Adela');
+-- 2 Muestra los datos de los comerciales que tengan como primer apellido Gómez o Domínguez.
+select * from comercial where apellido1 in ('Gómez', 'Domínguez');
+-- 3 Muestra los datos de los pedidos realizados por los clientes con los códigos 1, 2 y 6.
+select * from pedido where id_cliente in (1, 2, 6);
+-- 4 Muestra los datos de los pedidos realizados por los comerciales cuyo códigos no sean 1, 2 ó 6.
+select * from pedido where not id_comercial in (1, 2, 6);
 
 -- Base de datos NBA (4 tablas)
-
+use nba;
 -- 1 Muestra los nombres y las ciudades de los equipos cuya división sea la Central o la Pacific.
-
+select nombre, ciudad from equipos where division in ('Central', 'Pacific');
 -- 2 Muestra el jugador, puntos y asistencias por partido, de los jugadores que hayan realizado 3 ó 5 rebotes de media por partido.
-
+select jugador, Puntos_por_partido, Asistencias_por_partido from estadisticas where Rebotes_por_partido in (3,5);
 -- 3 Muestra el nombre y procedencia de los jugadores que procedan de Utah, Michigan, Villanova o Utah.
-
+select nombre, procedencia from jugadores where Procedencia in ('Utah', 'Michigan', 'Villanova', 'Utah');
 -- 4 Muestra los equipos locales que hayan logrado 74, 86, 90 ó 100 puntos.
+select equipo_local from partidos where puntos_local in (74, 86, 90, 100);
 
 
 -- Base de datos DepartamentoOficinas (5 tablas)
-
+use departamentooficinas;
 -- 1 Muestra el salario de las categorías de Administrativo y Comercial.
-
+select salario from categoria where titulo in ('Administrativo','Comercial');
 -- 2 Muestra los empleados de las categorías 1, 3 y 5.
-
+select salario from categoria where titulo in ('Administrativo','Comercial');
 -- 3 Muestra los datos de la oficina 11 que aparecen en la tabla dpto_oficinas.
-
+select * from dpto_oficinas where oficina in (11);
 
 -- Base de datos Jardinería (8 tablas)
 
 -- 1 Muestra el nombre del cliente y el del contacto, siendo la ciudad del cliente Barcelona, Madrid o Miami.
-
+select nombre_cliente, nombre_contacto from cliente where ciudad in ('Barcelona', 'Madrid', 'Miami');
 -- 2 Muestra el código de los productos vendidos cuyo precio por unidad sea 12, 15, 19 ó 25.
-
+select codigo_producto from detalle_pedido where precio_unidad in (12, 15, 19, 25);
 -- 3 Muestra nombre, apellidos, email y puesto de los empleados que no sean Director de oficina ni Representante de ventas.
-
+select nombre, apellido1, apellido2, email, puesto from empleado where not puesto in ('Director Oficina','Representante Ventas');
 -- 4 Muestra el tipo de gama de producto y la descripción de las gamas que no sean ni aromáticas ni ornamentales.
-
+select gama, descripcion_texto gama from gama_producto where not gama in ('Aromáticas','Ornamentales');
 -- 5 Muestra el código de las oficinas y de sus ciudades, ubicadas en Estados Unidos o en España.
-
+select codigo_oficina, ciudad from oficina where pais in ('España','EEUU');
 -- 6 Muestra el detalle de los pagos que no se hayan realizado por PayPal.
-
+select *  from pago where not forma_pago in ('PayPal');
 -- 7 Muestra el detalle de los pedidos que no se hayan entregado.
-
+select *  from pedido where not estado in ('entregado');
 -- 8 Muestra el detalle de los productos, salvo su código, de aquellos cuyo precio de venta sea 12, 15, 19 ó 25.
+select nombre, gama, dimensiones, proveedor, descripcion, cantidad_en_stock, precio_venta, precio_proveedor from producto where precio_venta in (12, 15, 19, 25);
--- 04 BASES DE DATOS/2_EVALUACION/UD 5. Realización de consultas/BD_Consulta/3_Consultas_con_operadores_de_rango_BETWEEN.sql
+++ 04 BASES DE DATOS/2_EVALUACION/UD 5. Realización de consultas/BD_Consulta/3_Consultas_con_operadores_de_rango_BETWEEN.sql
@@ -1,46 +1,70 @@
 -- Base de datos Tienda Informática (2 tablas)
 
 -- 1 Muestra los productos cuyo precio sea menor o igual a 120€.
+select nombre from producto where precio between 0 and 120;
 -- 2 Muestra los productos cuyo precio oscile entre los 80 y 300€.
+select nombre from producto where precio between 80 and 300;
 -- 3 Muestra los productos cuyo código de fabricante sea 1, 3 ó 5.
+select nombre from producto where codigo_fabricante =  1 or codigo_fabricante =  3 or codigo_fabricante =  5;
 -- 4 Muestra los productos del fabricante con el código 6 cuyo precio sea mayor a 200€.
-
+select nombre from producto where codigo_fabricante = 6 and precio >200;
 
 -- Base de datos Gestión de Empleados (2 tablas)
 
 -- 1 Muestra el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto mayor o igual a 150000 €.
+select nombre, presupuesto from departamento where presupuesto >= 150000;
 -- 2 Muestra el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto entre 100000 y 200000 €.
+select nombre, presupuesto from departamento where presupuesto between 100000 and 200000;
 -- 3 Muestra el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 €.
+select nombre from departamento where not presupuesto between 100000 and 200000;
 -- 4 Muestra el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean mayores que el presupuesto del que disponen.
-
+select nombre, gastos, presupuesto from departamento where gastos > presupuesto;
 
 -- Base de datos Gestión de Ventas (3 tablas)
 
 -- 1 Muestra los clientes cuyo número de categoría esté entre 150 y 250.
+select * from cliente where categoría between 150 and 250;
 -- 2 Muestra los comerciales cuya comisión sea superior al 12%.
+select * from comercial where comisión >0.12;
 -- 3 Muestra los pedidos cuyo total sea inferior a 200€.
+select * from pedido where total < 200;
 
 
 -- Base de datos NBA (4 tablas)
 
 -- 1 Muestra el jugador y la temporada de quienes hayan realizado entre 3 y 5 rebotes de media por partido.
+select jugador, temporada from estadisticas where Rebotes_por_partido between 3 and 5;
 -- 2 Muestra los nombres de los jugadores que pesen entre 180 y 230 libras.
+select nombre from jugadores where Peso between 180 and 230;
 -- 3 Muestra los equipos que hayan recibido entre 50 y 80 puntos del equipo rival.
+select equipo_local, equipo_visitante from partidos where puntos_visitante or puntos_local between 50 and 80;
 -- 4 Muestra los equipos que hayan recibido menos de 50 puntos del equipo rival.
-
+select equipo_local, equipo_visitante from partidos where puntos_visitante or puntos_local <50;
 
 -- Base de datos DepartamentoOficinas (5 tablas)
 
 -- 1 Muestra los empleados cuya categoría sea Director o Jefe de sección (códigos 1 y 2).
+select * from categoria where titulo = "Director" or titulo = "Jefe de sección" ;
+select * from empleado where categoria between 1 and 2 ;
 -- 2 Muestra los empleados de categoría Administrativo (código 3) cuya edad sea mayor a 35 años.
+select * from empleado where categoria = 3 and edad >35;
 -- 3 Muestra las categorías cuyo salario sea inferior a 35.000€ o superior a 40.000€.
-
+select categoria from categoria where salario <35000 or salario >40000;
+select * from categoria where salario <35000 or salario >40000;
 
 -- Base de datos Jardinería (8 tablas)
 
 -- 1 Muestra nombre y límite de crédito de los clientes que tengan un límite de crédito no superior a 35000 €.
--- 2 Muestra los detalles de los pedidos cuyo precio por unidad por unidad de producto oscile entre 12 y 24 €.
+select nombre_cliente, limite_credito from cliente where limite_credito < 35000;
+-- 2 Muestra los detalles de los pedidos cuyo precio por unidad de producto oscile entre 12 y 24 €.
+select * from detalle_pedido where precio_unidad between 12 and 24;
 -- 3 Muestra nombre, apellidos, email y puesto de trabajo de los empleados cuyos códigos de jefe no sean superiores a 3 ni inferiores a 20.
+-- ????????????????????????????????????????????????????????????
+select nombre, apellido1, apellido2, email, puesto from empleado where codigo_jefe < 3 and codigo_jefe >20; 
+-- ????????????????????????????????????????????????????????????
 -- 4 Muestra código y nombre de las oficinas cuyo código postal sea superior a 9000.
+select codigo_oficina, ciudad from oficina where codigo_postal >900;
 -- 5 Muestra los detalles de los pagos cuyo total sea superior a 500 € e inferior a 3000€.
+select * from pago where total > 500 and total < 3000;
 -- 6 Muestra los productos cuyos precios de venta oscilen entre los 8 y 10 €, y que su cantidad en el stock sea menor de 100.
+select * from producto where precio_venta between 8 and 10 and cantidad_en_stock < 100;
