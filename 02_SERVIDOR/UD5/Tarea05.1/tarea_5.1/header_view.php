76eeea2b2e1e5a62445e2fdc928d06b1231156a1
zono32 <masterzono32@gmail.com>
2024-01-26 20:44:51 +0100

    tarea 5.1 php

---
+++ 02_SERVIDOR/UD5/Tarea05.1/tarea_5.1/header_view.php
@@ -0,0 +1,19 @@
+<div class="float float-right d-inline-flex mt-2">
+        <i class="fa fa-shopping-cart mr-2 fa-2x"></i>
+        <?php
+        if (isset($_SESSION['cesta'])) {
+
+            $cantidad = count($_SESSION['cesta']);
+           // $multiplicador = $_POST['multiplicador'];
+           // var_dump($multiplicador);
+            echo "<input type='text' disabled class='form-control mr-2 bg-transparent text-white' value='$cantidad' size='2px'>";
+        } else {
+            echo "<input type='text' disabled class='form-control mr-2 bg-transparent text-white' value='0' size='2px'>";
+        }
+
+        ?>
+        <i class="fas fa-user mr-3 fa-2x"></i>
+        <input type="text" size='10px' value="<?php echo $_SESSION['nombre']; ?>" class="form-control
+    mr-2 bg-transparent text-white" disabled>
+        <a href="cerrar.php" class="btn btn-warning mr-2">Salir</a>
+    </div>
