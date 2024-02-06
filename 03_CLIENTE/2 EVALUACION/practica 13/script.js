2066d2d79c077e2ca6415c89a776e49955270f84
zono32 <masterzono32@gmail.com>
2024-02-06 21:58:10 +0100

    cliente

---
+++ 03_CLIENTE/2 EVALUACION/practica 13/script.js
@@ -0,0 +1,49 @@
+
+
+window.onload = () =>{
+    let numeros = [];
+    
+    function numerosAleatorios() {
+        while (numeros.length < 8) {
+            let numero = Math.floor(Math.random() * 16) + 1;
+            if (!numeros.includes(numero)) {
+                numeros.push(numero);
+            }
+        }       
+    }
+
+    numerosAleatorios();
+    console.log(numeros);
+
+    let numerosUsuario = [];
+
+    function numeroUsuario() {
+       for (let i = 0; i < 4; i++) {      
+           let userNumb = prompt("dime un número")
+           numerosUsuario.push(userNumb);
+       } 
+    }
+    
+    numeroUsuario();
+    console.log(numerosUsuario);
+
+    let vistaNumero = document.querySelectorAll('td');
+    console.log(vistaNumero);
+
+    
+    vistaNumero.forEach(element => {
+        console.log(element.id);
+        if (numeros.includes(parseInt(element.id))) {
+            element.innerText = element.id 
+        } 
+        if(includes(numerosUsuario) 
+       
+            
+    });
+
+   numeros.forEach(element => {
+    
+   });
+}
+
+
