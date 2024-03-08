cd6106264d0fdd9b37697b50ae2c6c1243d7c149
zono32 <masterzono32@gmail.com>
2024-03-08 17:27:33 +0100

    upload tarea 7.1

--- 02_SERVIDOR/UD7/Tarea 7.1/view/template/spa_view.php
+++ 02_SERVIDOR/UD7/Tarea 7.1/view/template/spa_view.php
@@ -80,9 +80,9 @@
 
     </section>
 
-    <script src="../js/global.js" type="text/javascript"></script>
-    <script src="../js/cargarDatos.js" type="text/javascript"></script>
-    <script src="../js/manejarSesion.js" type="text/javascript"></script>
+    <script src="../../js/global.js" type="text/javascript"></script>
+    <script src="../../js/cargarDatos.js" type="text/javascript"></script>
+    <script src="../../js/manejarSesion.js" type="text/javascript"></script>
 
 </body>
 
