b78fcdf982aee92c51b695edb4bec03c397d2be6
zono32 <masterzono32@gmail.com>
2024-04-05 22:05:24 +0200

    Update Planetas_ENUNCIADO.sql

--- 04 BASES DE DATOS/3_EVALUACION/UD-7/Tarea_1/Planetas_ENUNCIADO.sql
+++ 04 BASES DE DATOS/3_EVALUACION/UD-7/Tarea_1/Planetas_ENUNCIADO.sql
@@ -15,22 +15,22 @@ use planetas;
 #------------------------------------------------------------------------------------------
 drop table if exists planeta;
 create table planeta (
-	idPlaneta				int							AUTO_INCREMENT,
-    nombrePlaneta 			varchar( 20 ),
-    distancia 				int,
-    masa					int,
-    radio 					int,
+	idPlaneta				INT							AUTO_INCREMENT,
+    nombrePlaneta 			VARCHAR( 20 ),
+    distancia 				FLOAT,
+    masa					FLOAT,
+    radio 					FLOAT,
     
     Constraint		PK_Planeta						Primary Key( idPlaneta ),
     Constraint		UK_Nombre_Planeta				Unique Key( nombrePlaneta ),
     Constraint		Nombre_Planeta_NO_NULO			check( nombrePlaneta IS NOT NULL ),
     Constraint		Nombre_Planeta_NO_VACÍO			check( nombrePlaneta != '' ),
     Constraint		distancia_NO_NULO				check( distancia IS NOT NULL ),
-    Constraint		distancia_NO_VACÍO				check( distancia != '' ),
+    #Constraint		distancia_NO_VACÍO				check( distancia != '' ),
     Constraint		masa_NO_NULO					check( masa IS NOT NULL ),
-    Constraint		masa_NO_VACÍO					check( masa != '' ),
+    #Constraint		masa_NO_VACÍO					check( masa != '' ),
     Constraint		radio_NO_NULO					check( radio IS NOT NULL ),
-    Constraint		radio_NO_VACÍO					check( radio != '' ),    
+    #Constraint		radio_NO_VACÍO					check( radio != '' ),    
     Index 			idNombre_Planeta( nombrePlaneta )
 );
 
@@ -38,7 +38,7 @@ create table planeta (
 #------------------------------------------------------------------------------------------
 #	AGREGAMOS LOS DATOS DEL Sol y de los ocho Planetas
 #------------------------------------------------------------------------------------------
-	INSERT INTO planeta( nombrePlaneta, distancia, masa, radio )	VALUES( 'Sol', 0, 1.9891e30, 695500000);
+	INSERT INTO planeta( nombrePlaneta, distancia, masa, radio )	VALUES( 'Sol', 1, 1.9891e30, 695500000);
     INSERT INTO planeta( nombrePlaneta, distancia, masa, radio )	VALUES( 'Mercurio',57900000000 , 1.3e23, 2439700);
     INSERT INTO planeta( nombrePlaneta, distancia, masa, radio )	VALUES( 'Venus', 108200000000, 4.87e24, 6051800);
     INSERT INTO planeta( nombrePlaneta, distancia, masa, radio )	VALUES( 'Tierra', 149600000000, 5.97e24, 6371000);
@@ -65,6 +65,18 @@ create table planeta (
 #
 #
 #------------------------------------------------------------------------------------------
+DELIMITER //
+	DROP PROCEDURE IF EXISTS gravedadPlaneta //
+    CREATE function calcularGravedad(masa FLOAT, radio FLOAT)
+    RETURNS FLOAT
+		BEGIN
+            DECLARE G FLOAT;
+			SET G = 6.67430e-11;
+			RETURN (G * masa) / POWER(radio, 2);
+        END
+// DELIMITER ;
+
+
 #------------------------------------------------------------------------------------------
 #	Creamos un procedimiento o función para obtener la velocidad de escape de la superficie
 #	  del cuerpo
@@ -95,14 +107,23 @@ create table planeta (
 #	Creamos una vista que muestre el nombre del Planeta, Distancia al sol (m), Masa (Kg), Radio (m), Gravedad ecuatorial, Velocidad de escape (m/s), Velocidad Orbital
 #		CREATE VIEW DatosPlanetas AS SELECT 
 #------------------------------------------------------------------------------------------
-
-
+DROP VIEW IF EXISTS DatosPlanetas;
+CREATE VIEW DatosPlanetas AS 
+SELECT 
+    nombrePlaneta AS 'Planeta',
+    distancia AS 'Distancia al sol (m)',
+    masa AS 'Masa (kg)',
+    radio AS 'Radio (m)',
+    calcularGravedad(masa, radio) AS 'Gravedad ecuatorial'
+   # calcularVelocidadEscape(masa, radio) AS 'Velocidad de escape (m/s)',
+   # calcularVelocidadOrbital(masa, radio) AS 'Velocidad Orbital'
+FROM Planeta;
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 
-set @masa = (SELECT masa FROM Planeta WHERE nombre = 'Tierra');
-set @radio = (SELECT radio FROM Planeta WHERE nombre = 'Tierra' );
-set @distancia = (SELECT distancia FROM Planeta WHERE nombre = 'Tierra' );
+set @masa = (SELECT masa FROM Planeta WHERE nombrePlaneta = 'Tierra');
+set @radio = (SELECT radio FROM Planeta WHERE nombrePlaneta = 'Tierra' );
+set @distancia = (SELECT distancia FROM Planeta WHERE nombrePlaneta = 'Tierra' );
 SELECT calcularGravedad( @masa, @radio );
-SELECT calcularVelocidadEscape( @masa, @radio );
+#SELECT calcularVelocidadEscape( @masa, @radio );
 SELECT * FROM DatosPlanetas;
